691ea5dcda118f5c15089ca50462e41522c23f36
techacademy.lesson <kthrowaway0001@gmail.com>
2022-08-04 14:55:21 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,17 @@
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
-                        {{-- タスク作成ページへのリンク --}}
-                        <li>{!! link_to_route('tasks.create', '新規タスク追加', [], ['class' => 'nav-link']) !!}</li>
-                        {{-- ユーザ登録ページへのリンク --}}
-                        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-                        {{-- ログインページへのリンク --}}
-                        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                        @if (Auth::check())
+                            {{-- タスク作成ページへのリンク --}}
+                            <li>{!! link_to_route('tasks.create', '新規タスク追加', [], ['class' => 'nav-link']) !!}</li>
+                            {{-- ログアウトへのリンク --}}
+                            <li">{!! link_to_route('logout.get', 'Logout', [], ['class' => 'nav-link']) !!}</li>
+                        @else
+                            {{-- ユーザ登録ページへのリンク --}}
+                            <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                            {{-- ログインページへのリンク --}}
+                            <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                        @endif
                     </ul>
                 </div>
             </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,30 +2,41 @@
 
 @section('content')
 
-    <h1>タスク一覧</h1>
+    @if (Auth::check())
+        <h1>タスク一覧</h1>
+        
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>ステータス</th>
+                        <th>タスク</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
     
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>ステータス</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        {{-- タスク作成ページへのリンク --}}
+        {!! link_to_route('tasks.create', '新規タスク追加', [], ['class' => 'btn btn-primary']) !!}
+    
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklist</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
     @endif
-    
-    {{-- タスク作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスク追加', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
